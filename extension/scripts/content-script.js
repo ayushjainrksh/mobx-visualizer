2055d43e8e8b872fe0906a7299b68972c5215135
Ayush Jain <ayushjainrksh@gmail.com>
2023-08-20 18:28:06 +0530

    Minor change

--- extension/scripts/content-script.js
+++ extension/scripts/content-script.js
@@ -9,11 +9,11 @@ function injectScript(file_path, tag) {
 // eslint-disable-next-line no-undef
 injectScript(chrome.runtime.getURL('scripts/inject-script.js'), 'body');
 
-window.addEventListener("message", function(event) {
+window.addEventListener('message', function(event) {
     if(event.data.type && event.data.type==='FROM_PAGE') {
         if(event?.data?.mobxVisualizerData?.mobxVisualizer) {
             // eslint-disable-next-line no-undef
-            chrome.runtime.sendMessage({mobxVisualizerData: event.data.mobxVisualizerData});
+            chrome.runtime.sendMessage(chrome.runtime.id, {mobxVisualizerData: event.data.mobxVisualizerData});
         }
     }
 }, false)

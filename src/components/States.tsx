242cb0c8219975bce108f08d3fc3792e8736a4e6
Ayush Jain <ayush.jain@deel.com>
2026-02-26 14:27:49 +0530

    Update dependencies

--- src/components/States.tsx
+++ src/components/States.tsx
@@ -6,15 +6,15 @@ import './styles/States.css';
 const sampleData = {
   mobxVisualizer: {
     sampleStore: {
-      storeData: [
-        'abc'
-      ]
-    }
-  }
+      storeData: ['abc'],
+    },
+  },
 };
 
-const States = (): JSX.Element => {
-  const [mobxStore, setMobxStore] = useState<Record<string, any>>(sampleData.mobxVisualizer);
+const States = (): React.JSX.Element => {
+  const [mobxStore, setMobxStore] = useState<Record<string, any>>(
+    sampleData.mobxVisualizer,
+  );
   const [filters, setFilters] = useState<string>('');
   const [searchValue, setSearchValue] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -22,30 +22,42 @@ const States = (): JSX.Element => {
   const filteredStores = filters ? mobxStore[filters] : mobxStore;
 
   useEffect(() => {
-    chrome?.runtime?.onMessage && chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-      if (message?.mobxVisualizerData?.mobxVisualizer) {
-        chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([tab]) => {
-          if (tab.id === sender?.tab?.id) {
-            setMobxStore(parse(message?.mobxVisualizerData?.mobxVisualizer));
-            setIsLoading(false);
-          }
-        });
-      }
-    });
+    chrome?.runtime?.onMessage &&
+      chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        if (message?.mobxVisualizerData?.mobxVisualizer) {
+          chrome.tabs.query(
+            { active: true, lastFocusedWindow: true },
+            ([tab]) => {
+              if (tab.id === sender?.tab?.id) {
+                setMobxStore(
+                  parse(message?.mobxVisualizerData?.mobxVisualizer),
+                );
+                setIsLoading(false);
+              }
+            },
+          );
+        }
+      });
   }, []);
 
-  const renderStores = (): JSX.Element[] => {
+  const renderStores = (): React.JSX.Element[] => {
     const storeList = [];
     for (const store in mobxStore) {
-      if (searchValue && !store.toLowerCase().includes(searchValue.toLowerCase())) continue;
+      if (
+        searchValue &&
+        !store.toLowerCase().includes(searchValue.toLowerCase())
+      )
+        continue;
 
       storeList.push(
         <div
           className={`mv-states__stores__item ${filters === store ? 'mv-states__stores__item--selected' : ''}`}
-          onClick={() => { setFilters(store); }}
+          onClick={() => {
+            setFilters(store);
+          }}
         >
           {store}
-        </div>
+        </div>,
       );
     }
 
@@ -54,43 +66,50 @@ const States = (): JSX.Element => {
 
   return (
     <div className='mv-states'>
-      {
-        isLoading
-          ? <div className='mv-states__loading'>
-            <img src='/logo192.png' alt='Mobx Visualizer logo'/>
-            <div className='mv-states__loading__text'>
-              <span className='mv-states__loading__text__title'>Connecting Mobx store...</span>
-              <span className='mv-states__loading__text__subtitle'>Please refresh your tab if it takes too long to connect</span>
-            </div>
+      {isLoading ? (
+        <div className='mv-states__loading'>
+          <img src='/logo192.png' alt='Mobx Visualizer logo' />
+          <div className='mv-states__loading__text'>
+            <span className='mv-states__loading__text__title'>
+              Connecting Mobx store...
+            </span>
+            <span className='mv-states__loading__text__subtitle'>
+              Please refresh your tab if it takes too long to connect
+            </span>
           </div>
-          : <>
-            <div className='mv-states__stores'>
-              {
-                <div className='mv-states__stores__action' onClick={() => { setFilters(''); }}>
-                  {
-                    filters
-                      ? 'Clear filter'
-                      : 'Filter stores'
-                  }
-                </div>
-              }
-              {
-                <div className='mv-states__stores__search'>
-                  <input
-                    type='text'
-                    placeholder='Search stores'
-                    value={searchValue}
-                    onChange={(e) => { setSearchValue(e.target.value); }}
-                  />
-                </div>
-              }
-              {renderStores()}
-            </div>
-            <div className='mv-states__json-view'>
-              <JSONTree data={filteredStores} />;
-            </div>
-          </>
-      }
+        </div>
+      ) : (
+        <>
+          <div className='mv-states__stores'>
+            {
+              <div
+                className='mv-states__stores__action'
+                onClick={() => {
+                  setFilters('');
+                }}
+              >
+                {filters ? 'Clear filter' : 'Filter stores'}
+              </div>
+            }
+            {
+              <div className='mv-states__stores__search'>
+                <input
+                  type='text'
+                  placeholder='Search stores'
+                  value={searchValue}
+                  onChange={(e) => {
+                    setSearchValue(e.target.value);
+                  }}
+                />
+              </div>
+            }
+            {renderStores()}
+          </div>
+          <div className='mv-states__json-view'>
+            <JSONTree data={filteredStores} />;
+          </div>
+        </>
+      )}
     </div>
   );
 };
